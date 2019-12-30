e1bebbad72982bf334849bbe4df3f5b844f49b50
Ryan Miller <ryan@intranoggin.com>
2019-12-30 13:08:59 -0500

    Point at project DB instead of scaffold source DB

--- Database/Context/GDomContext.cs
+++ Database/Context/GDomContext.cs
@@ -23,8 +23,7 @@ namespace Database.Context
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=(local); Database=GlobalDomination;integrated security=True");
+                optionsBuilder.UseSqlServer("Server=(localdb)\\ProjectsV13; Database=GlobalDomination;integrated security=True");
             }
         }
 
